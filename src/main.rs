f60189416fb0cdde178d69a82799d5a8c771df08
qifu2022 <qifu2022@gmail.com>
2025-03-23 16:55:49 +0800

    third commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 fn main() {
-    println!("I love China!");
-     println!("Hello, world!");
-     println!("lucky is a lovely dog");
+    println!("{},{}",first_word(&String::from("123")),largest_num(&[1,2,3]));
+}
+
+//获取字符串长度
+pub fn first_word(s:&String)->usize {
+    let bytes=s.as_bytes();
+    for (i,&item) in bytes.iter().enumerate() {
+        if item==b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+//获取数组最大值
+fn largest_num<T:PartialOrd>(list:&[T])->&T{
+    let mut max=&list[0];
+    for num in list.iter() {
+        if num>max {
+            max=num;
+        }
+    }
+    max
 }

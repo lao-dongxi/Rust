7ecb87b7dea64ed64444b5bbc1feac2f4e5e0c3f
qifu2022 <qifu2022@gmail.com>
2025-03-30 21:01:14 +0800

    gcd

---
+++ src/bin/gcd.rs
@@ -0,0 +1,29 @@
+use std::{env,str::FromStr};
+fn main() {
+    //
+    let mut numbers=Vec::new();
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("err parsing argument"));
+    }
+    if numbers.len()==0 {
+        eprintln!("Usage:gcd NUMBER...");
+        std::process::exit(1);
+    }
+    let mut d=numbers[0];
+    for i in &numbers[1..] {
+        d=gcd(d, *i);
+    }
+    println!("the greatest common divisor of {:?} is {}",numbers,d);
+    
+}
+
+fn gcd(mut m:u64,mut n:u64)->u64{
+    assert!(m!=0&&n!=0);
+    while n!=0 {
+        let temp=m%n;
+        m=n;
+        n=temp;
+        // println!("{}",n);
+    }
+    m
+}
